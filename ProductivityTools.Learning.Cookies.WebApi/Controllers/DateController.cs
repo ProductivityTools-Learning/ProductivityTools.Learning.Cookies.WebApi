f382197defa5fc1b171fd7969bca4d0eca485401
Pawel Wujczyk <pwujczyk@hotmail.com>
2023-06-30 15:41:53 +0200

    hosting image

---
+++ ProductivityTools.Learning.Cookies.WebApi/Controllers/DateController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProductivityTools.Learning.Cookies.WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DateController : ControllerBase
+    {
+        [HttpGet]
+        [Route("Get")]
+        public string Index()
+        {
+            var cookieOptions = new CookieOptions();
+            cookieOptions.Expires = DateTime.Now.AddDays(1);
+            //cookieOptions.Path = "/";
+            Response.Cookies.Append("SomeCookie", "SomeValue", cookieOptions);
+
+            return DateTime.Now.ToString();
+        }
+
+        private const string MimeType = "image/png";
+        private const string FileName = "CM-Logo.png";
+
+        [HttpGet]
+        [Route("Image")]
+        public IActionResult Get(string fileName)
+        {
+            var ookies = Request.Cookies;
+            string value = string.Empty;
+            ookies.TryGetValue("SomeCookie", out value);
+          
+            FileStream file = new FileStream("image.png", FileMode.Open);
+
+            return File(file, MimeType, FileName);
+        }
+    }
+}
--- ProductivityTools.Learning.Cookies.WebApi/Program.cs
+++ ProductivityTools.Learning.Cookies.WebApi/Program.cs
@@ -1,9 +1,18 @@
+using Microsoft.Extensions.Options;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("MyAllowSpecificOrigins",
+    builder =>
+    {
+        builder.WithOrigins("http://localhost:3000", "https://meetingsweb.z13.web.core.windows.net").AllowAnyMethod().AllowAnyHeader();
+    });
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
